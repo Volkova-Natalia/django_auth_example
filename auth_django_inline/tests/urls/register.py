409cdf98045d9d9f30f9c791d6b904f7b7b2b762
Volkova-Natalia <volkova.natalia.1988@gmail.com>
2020-06-09 15:52:59 +0300

    (+) tests/urls заглушки для нужных url.

---
+++ auth_django_inline/tests/urls/register.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+from django.urls import reverse, resolve
+
+from ...urls import base_url, namespace
+
+
+# Create your tests here.
+class RegisterTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        pass
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    # ======================================================================
+
+    # ======================================================================
