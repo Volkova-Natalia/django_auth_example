3975c067ba806c1b0e4fe71d02c79f903ca39dc7
Volkova-Natalia <volkova.natalia.1988@gmail.com>
2020-07-27 08:28:32 +0300

    (refactor) clean -> success, dirty -> fail in all tests (views/login).

--- auth_django_inline/tests/views/login.py
+++ auth_django_inline/tests/views/login.py
@@ -34,66 +34,73 @@ class LoginTestCase(TestCase):
         user.save()
 
     # ======================================================================
-    # clean
+    # success
     # ======================================================================
 
     # ----- GET -----
 
-    def test_get_clean(self):
+    def test_get_success(self):
+        success_fail = 'success'
+
         login = BaseLoginTestCase()
         client, response = login.get(client=None)
-        login._test_get(response, success_fail='success', assert_message='views')
+        login._test_get(response, success_fail=success_fail, assert_message='views')
 
     # ----- POST -----
 
-    def test_post_successful_clean(self):
+    def test_post_success(self):
+        success_fail = 'success'
+
         self._create_user(self.registered_user)
         data_post = self.registered_user.copy()
 
         login = BaseLoginTestCase(data_post)
         client, response = login.post(client=None)
-        login._test_post(response, success_fail='success', assert_message='views')
-
-    def test_post_user_is_not_correct_clean(self):
-        self._create_user(self.registered_user)
-        data_post = self.registered_user.copy()
-        data_post['username'] = data_post['username'] + '_another'
-
-        login = BaseLoginTestCase(data_post)
-        client, response = login.post(client=None)
-        login._test_post(response, success_fail='fail', assert_message='views')
-
-    def test_post_password_is_not_correct_clean(self):
-        self._create_user(self.registered_user)
-        data_post = self.registered_user.copy()
-        data_post['password'] = data_post['password'] + '_another'
-
-        login = BaseLoginTestCase(data_post)
-        client, response = login.post(client=None)
-        login._test_post(response, success_fail='fail', assert_message='views')
-
-    def test_post_user_password_is_not_correct_clean(self):
-        self._create_user(self.registered_user)
-        data_post = self.registered_user.copy()
-        data_post['username'] = data_post['username'] + '_another'
-        data_post['password'] = data_post['password'] + '_another'
-
-        login = BaseLoginTestCase(data_post)
-        client, response = login.post(client=None)
-        login._test_post(response, success_fail='fail', assert_message='views')
+        login._test_post(response, success_fail=success_fail, assert_message='views')
 
     # ======================================================================
-    # dirty
+    # fail
     # ======================================================================
 
     # ----- GET -----
 
-    def test_get_dirty(self):
+    def test_get_fail(self):
         pass
 
     # ----- POST -----
 
-    def test_post_dirty(self):
-        pass
+    def test_post_user_is_not_correct_fail(self):
+        success_fail = 'fail'
+
+        self._create_user(self.registered_user)
+        data_post = self.registered_user.copy()
+        data_post['username'] = data_post['username'] + '_another'
+
+        login = BaseLoginTestCase(data_post)
+        client, response = login.post(client=None)
+        login._test_post(response, success_fail=success_fail, assert_message='views')
+
+    def test_post_password_is_not_correct_fail(self):
+        success_fail = 'fail'
+
+        self._create_user(self.registered_user)
+        data_post = self.registered_user.copy()
+        data_post['password'] = data_post['password'] + '_another'
+
+        login = BaseLoginTestCase(data_post)
+        client, response = login.post(client=None)
+        login._test_post(response, success_fail=success_fail, assert_message='views')
+
+    def test_post_user_password_is_not_correct_fail(self):
+        success_fail = 'fail'
+
+        self._create_user(self.registered_user)
+        data_post = self.registered_user.copy()
+        data_post['username'] = data_post['username'] + '_another'
+        data_post['password'] = data_post['password'] + '_another'
+
+        login = BaseLoginTestCase(data_post)
+        client, response = login.post(client=None)
+        login._test_post(response, success_fail=success_fail, assert_message='views')
 
     # ======================================================================
